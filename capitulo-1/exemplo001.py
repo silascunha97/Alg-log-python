b1c74d6abe572bf9c86a4daa4ae15437e65543f1
Silas Augusto da Cunha <0997s.a.cunha@gmail.com>
2024-02-28 00:12:07 -0300

    Aula entendendo variaveis e operadores aritmética

---
+++ capitulo-1/exemplo001.py
@@ -0,0 +1,15 @@
+# Programa 2.1 - Primeiro programa com variaveis.
+valorA = 2
+valorB = 3
+print(valorA+valorB)
+# Fim do Programa 2.1 - Primeiro programa com variaveis.
+
+# Programa 2.1 - Primeiro programa com variaveis.
+salario = 1500
+aumento = 5
+print(salario + (salario  * aumento / 100))
+# Fim do Programa 2.1 - Aumentando o salário.
+
+# Programa 2.2 -Alternativa para o cálculo de aumento de salario.
+print(1500 + (1500 * 5 / 100))
+# Fim Programa 2.2 -Alternativa para o cálculo de aumento de salario.
